089e9fb27f0caf5500601b819116601655399dfc
SoZaynDigitaL <sozaynd@gmail.com>
2025-05-23 05:07:06 -0400

    GitHub Actions workflow

--- my-app/web/next.config.js
+++ my-app/web/next.config.js
@@ -1,18 +1,7 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en',
-  },
   output: 'export',
-  trailingSlash: true,
-  reactStrictMode: true,
-  telemetry: false,
-  images: {
-    unoptimized: true
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
